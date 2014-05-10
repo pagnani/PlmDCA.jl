58b3f0c33ee68a6417dea90c89f8b729031d4864
Andrea Pagnani <andrea.pagnani@gmail.com>
2014-05-11 01:05:04 +0200

    possibly working single site update. Simple gradient descent

--- src/plmdca.jl
+++ src/plmdca.jl
@@ -1,13 +1,14 @@
 using GaussDCA
 
-#include("read_fasta_alignment.jl")
-
 function plmdca(nomefile::String; 
                 max_gap_fraction::Real = 0.9 , 
                 theta = :auto, 
                 lambdaJ::Real=0.005, 
                 lambdaH::Real=0.01,
-                epsconv::Real=0.1)
+                epsconv::Real=0.1,
+                maxit::Int=10,
+                epsgrad::Real=1e-5,
+                epsval::Real=1e-5)
     
     Z = GaussDCA.read_fasta_alignment(nomefile, max_gap_fraction)
     N, M = size(Z)
@@ -30,16 +31,30 @@ function plmdca(nomefile::String;
     # function gradPL!(vecJ)
     #     gradPLsite!(vecJ, site, Zint, W, q, lambdaH, lambdaJ, grad)
     # end
-
-    while true
+    normgrad = 10
+    while true && it <= maxit && normgrad > epsgrad
         site = 1
         val = PLsiteAndGrad!(vecJ, site, Zint, W, q, lambdaH, lambdaJ, grad)
-        vecJ += epsconv * grad
-        println("it ", it, " |grad| = ", sum(vecJ.*vecJ), " val = ", val)
+        vecJ -= epsconv * grad
+        normgrad = sum(grad .* grad)
+        println("it ", it, " |grad| = ", normgrad, " val = ", val)
         it += 1
     end
-
+    return Z, vecJ
 end
+
+# function converge()
+
+#     normgrad = 10
+#     while true && it <= maxit && normgrad > epsgrad
+#         site = 1
+#         val = PLsiteAndGrad!(vecJ, site, Zint, W, q, lambdaH, lambdaJ, grad)
+#         vecJ -= epsconv * grad
+#         normgrad = sum(grad .* grad)
+#         println("it ", it, " |grad| = ", normgrad, " val = ", val)
+#         it += 1
+#     end
+# end
 
 function PLsiteAndGrad!(vecJ::Array{Float64,1}, site::Int, Z::Array{Int,2}, W::Array{Float64,1}, q::Int, lambdaH::Float64, lambdaJ::Float64, grad::Array{Float64,1} )
 
@@ -48,70 +63,70 @@ function PLsiteAndGrad!(vecJ::Array{Float64,1}, site::Int, Z::Array{Int,2}, W::A
     N = size(Z,1)
 
     q2 = q*q
-
+    
     for i=1:LL-q
         grad[i] = 2.0 * lambdaJ * vecJ[i]
     end
     for i=(LL-q+1):LL
-        grad[i] = 2.0 * lambdaH * vecJ[i]
-    end
+       grad[i] = 2.0 * lambdaH * vecJ[i]
+    end 
 
     vecene = zeros(Float64,q)
     expvecenesunorm = zeros(Float64,q)
     pseudolike = 0.0
-    for a = 1:M
-        fillvecene!(vecJ, Z, a, site, N, q, vecene)        
-        norm = sumexp(vecene)
-        expvecenesunorm = exp(vecene) / norm
-        pseudolike -= W[a] * ( vecene[Z[site,a]] - log(norm) )
-        offset = 0
-        for i=1:site-1
-            for s = 1:q
-                grad[ offset + s + q * ( Z[i,a] - 1 ) ] -= W[a] * ( ID(Z[site,a],  s)  -  expvecenesunorm[s])
+    myrange = Int[tuple((1:site-1)...,(site+1:N)...)...] # concatenate tuples to vector 
+    # the cast to Int is relevant: factor 10x in speed
+    @inbounds begin 
+        for a = 1:M       
+            fillvecene!(vecJ, Z, a, myrange, N, q, vecene)        
+            norm = sumexp(vecene)
+            expvecenesunorm = exp(vecene .- log(norm))
+            #expvecenesunorm = exp(vecene)/norm
+            pseudolike -= W[a] * ( vecene[Z[site,a]] - log(norm) )
+            offset = 0
+            
+            
+            for i = myrange
+                @simd for s = 1:q
+                    grad[ offset + s + q * ( Z[i,a] - 1 ) ] -= W[a] * ( ID(Z[site,a],  s)  -  expvecenesunorm[s])
+                end
+                offset += q2 
             end
-            offset += q2 
-        end
-        for i=site+1:N
-            for s = 1:q
-                grad[ offset + s + q * ( Z[i,a] - 1 ) ] -= W[a] * ( ID(Z[site,a],  s)  -  expvecenesunorm[s])
+            for s = 1:q 
+                grad[ offset + s ] -= W[a] * ( ID(Z[site,a],s) - expvecenesunorm[s] )
             end
-            offset += q2 
-        end        
-        for s = 1:q 
-            grad[ offset + s ] -= W[a] * ( ID(Z[site,a],s) - expvecenesunorm[s] )
         end
     end
-
+#    println(pseudolike)
     pseudolike += L2norm(vecJ, lambdaJ, lambdaH, q)
     return pseudolike 
 end
 
-function fillvecene!(vecJ::Array{Float64,1}, Z::Array{Int,2}, a::Int, site::Int, N::Int, q::Int, vecene::Array{Float64,1})
+function fillvecene!(vecJ::Array{Float64,1}, Z::Array{Int,2}, a::Int, myrange::Array{Int,1}, N::Int, q::Int, vecene::Array{Float64,1})
 
     q2 = q*q
 
-    for alpha = 1:q
-        offset = 0
-        scra = 0.0
-        for i = 1:site-1
-            scra += vecJ[offset + alpha + q * (Z[i,a]-1)] #sum up to site - 1
-            offset += q2           
-        end
-        for i = site+1:N
-            scra += vecJ[offset + alpha + q * (Z[i,a]-1)] # from site + 1 to end
+    @inbounds begin
+        for l = 1:q
+            offset = 0
+            scra = 0.0
+            for i = myrange  # sum J
+                scra += vecJ[offset + l + q * (Z[i,a]-1)] 
             offset += q2 
+            end
+            scra += vecJ[offset + l] # sum H 
+            vecene[l] = scra
         end
-        scra += vecJ[offset + alpha] # sum external field 
-        vecene[alpha] = scra
     end
-
 end
 
 function sumexp(vec::Array{Float64,1})
 
     mysum = 0.0
-    for i=1:length(vec)
-        mysum += exp(vec[i])
+    @inbounds begin
+        for i=1:length(vec)
+            mysum += exp(vec[i])
+        end
     end
     return mysum
 end
