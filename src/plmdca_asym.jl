897b43fd91476efe9c397e6190030c36b5407e34
Andrea Pagnani <andrea.pagnani@gmail.com>
2020-05-24 20:48:15 +0200

    moved ComputeScore into main plmdca_asym file

--- src/plmdca_asym.jl
+++ src/plmdca_asym.jl
@@ -17,17 +17,17 @@ function plmdca_asym(Z::Array{T,2},W::Vector{Float64};
     N,M = size(Z)
     M = length(W)
     q = Int(maximum(Z))
-    
+
     plmalg = PlmAlg(method,verbose, epsconv ,maxit)
     plmvar = PlmVar(N,M,q,q*q,lambdaJ,lambdaH,Z,W)
     Jmat, pslike = if decimation  == false
-        MinimizePLAsym(plmalg,plmvar)                
+        MinimizePLAsym(plmalg,plmvar)
     else
-        decvar = DecVar{2}(fracdec, fracmax, ones(Bool, (N-1)*q*q, N)) 
-        DecimateAsym!(plmvar, plmalg, decvar)        
+        decvar = DecVar{2}(fracdec, fracmax, ones(Bool, (N-1)*q*q, N))
+        DecimateAsym!(plmvar, plmalg, decvar)
     end
     score, FN, Jtensor, htensor =  ComputeScore(Jmat, plmvar, min_separation)
-    return PlmOut(sdata(pslike), Jtensor, htensor, score)            
+    return PlmOut(sdata(pslike), Jtensor, htensor, score)
 
 end
 plmdca(Z,W;kwds...) = plmdca_asym(Z,W;kwds...)
@@ -44,12 +44,12 @@ end
 plmdca(filename::String; kwds...) = plmdca_asym(filename; kwds...)
 
 function MinimizePLAsym(alg::PlmAlg, var::PlmVar)
-    
+
     LL = (var.N - 1) * var.q2 + var.q
     x0 = zeros(Float64, LL)
     vecps = SharedArray{Float64}(var.N)
     Jmat = @distributed hcat for site=1:var.N #1:12
-        
+
         opt = Opt(alg.method, length(x0))
         ftol_abs!(opt, alg.epsconv)
         maxeval!(opt, alg.maxit)
@@ -59,7 +59,7 @@ function MinimizePLAsym(alg::PlmAlg, var::PlmVar)
         alg.verbose && println("exit status = $ret")
         vecps[site] = minf
         minx
-    end 
+    end
     return Jmat, vecps
 end
 
@@ -73,19 +73,19 @@ function ComputeUL(alg::PlmAlg, var::PlmVar, site::Int, LL::Int)
     offset::Int = 0
 
     for i=1:site-1
-        for s = 1:q2            
+        for s = 1:q2
             lb[offset + s] = -tiny
             ub[offset + s] =  tiny
         end
     end
-    offset += q2 
-   
+    offset += q2
+
     for i=site+1:N
-        for s = 1:q2            
+        for s = 1:q2
             lb[offset + s] = -tiny
             ub[offset + s] =  tiny
         end
-        offset += q2 
+        offset += q2
     end
     return lb,ub
 end
@@ -105,71 +105,71 @@ function PLsiteAndGrad!(vecJ::Array{Float64,1},  grad::Array{Float64,1}, site::I
     end
     for i=(LL-q+1):LL
        grad[i] = 4.0 * plmvar.lambdaH * vecJ[i]
-    end 
+    end
 
     vecene = zeros(Float64,q)
     expvecenesunorm = zeros(Float64,q)
     pseudolike = 0.0
- 
-    @inbounds for a = 1:M       
-        fillvecene!(vecene, vecJ,site,a, q, Z,N)        
+
+    @inbounds for a = 1:M
+        fillvecene!(vecene, vecJ,site,a, q, Z,N)
         lnorm = log(sumexp(vecene))
         expvecenesunorm .= exp.(vecene .- lnorm)
         pseudolike -= W[a] * (vecene[Z[site,a]] - lnorm)
         offset = 0
         fact = 1.0
-        for i = 1:site-1 
+        for i = 1:site-1
             @simd for s = 1:q
                 grad[ offset + s + q * ( Z[i,a] - 1 ) ] += fact * W[a] *  expvecenesunorm[s]
             end
-            grad[ offset + Z[site,a] + q * ( Z[i,a] - 1 ) ] -= fact*W[a] 
-            offset += q2 
+            grad[ offset + Z[site,a] + q * ( Z[i,a] - 1 ) ] -= fact*W[a]
+            offset += q2
         end
-	for i = site+1:N 
+	for i = site+1:N
             @simd for s = 1:q
                 grad[ offset + s + q * ( Z[i,a] - 1 ) ] += fact * W[a] *  expvecenesunorm[s]
             end
-            grad[ offset + Z[site,a] + q * ( Z[i,a] - 1 ) ] -= fact *W[a] 
-            offset += q2 
+            grad[ offset + Z[site,a] + q * ( Z[i,a] - 1 ) ] -= fact *W[a]
+            offset += q2
         end
-        
-        @simd for s = 1:q 
-            grad[ offset + s ] += W[a] *  expvecenesunorm[s] 
+
+        @simd for s = 1:q
+            grad[ offset + s ] += W[a] *  expvecenesunorm[s]
         end
-	grad[ offset + Z[site,a] ] -= W[a] 	
+	grad[ offset + Z[site,a] ] -= W[a]
     end
-    
+
     pseudolike += L2norm_asym(vecJ, plmvar)
 
-    return pseudolike 
+    return pseudolike
 end
 
 function fillvecene!(vecene::Array{Float64,1}, vecJ::Array{Float64,1},site::Int, a::Int, q::Int, sZ::DenseArray{Int,2},N::Int)
 
-    q2 = q*q   
+    q2 = q*q
     Z = sdata(sZ)
 
     @inbounds for l = 1:q
         offset::Int = 0
         scra::Float64 = 0.0
         for i = 1:site-1 # Begin sum_i \neq site J
-            scra += vecJ[offset + l + q * (Z[i,a]-1)] 
-            offset += q2 
+            scra += vecJ[offset + l + q * (Z[i,a]-1)]
+            offset += q2
         end
         # skipping sum over residue site
     	for i = site+1:N
-            scra += vecJ[offset + l + q * (Z[i,a]-1)] 
-            offset += q2 
+            scra += vecJ[offset + l + q * (Z[i,a]-1)]
+            offset += q2
         end # End sum_i \neq site J
 
-        scra += vecJ[offset + l] # sum H 
+        scra += vecJ[offset + l] # sum H
         vecene[l] = scra
     end
     return
 end
 
 function L2norm_asym(vec::Array{Float64,1}, plmvar::PlmVar)
-    q = plmvar.q    
+    q = plmvar.q
     N = plmvar.N
     lambdaJ = plmvar.lambdaJ
     lambdaH = plmvar.lambdaH
@@ -189,4 +189,51 @@ function L2norm_asym(vec::Array{Float64,1}, plmvar::PlmVar)
     mysum2 *= 2lambdaH
 
     return mysum1+mysum2
+end
+
+function ComputeScore(Jmat::Array{Float64,2}, var::PlmVar, min_separation::Int)
+
+    q = var.q
+    N = var.N
+
+    JJ=reshape(Jmat[1:end-q,:], q,q,N-1,N)
+    Jtemp1=zeros( q,q,Int(N*(N-1)/2))
+    Jtemp2=zeros( q,q,Int(N*(N-1)/2))
+    l = 1
+    for i=1:(N-1)
+        for j=(i+1):N
+            Jtemp1[:,:,l]=JJ[:,:,j-1,i]; #J_ij as estimated from from g_i.
+            Jtemp2[:,:,l]=JJ[:,:,i,j]'; #J_ij as estimated from from g_j.
+            l=l+1;
+        end
+    end
+
+
+
+
+    hplm = fill(0.0, q,N)
+    for i in 1:N
+        hplm[:,i] .= Jmat[end-q+1:end,i]
+    end
+
+    Jtensor1 = inflate_matrix(Jtemp1,N)
+    Jtensor2 = inflate_matrix(Jtemp2,N)
+    Jplm = (Jtensor1 + Jtensor2)/2 # for the energy I do not want to gauge
+
+    ctr = 0
+    for i in 1:N-1
+        for j in i+1:N
+            ctr += 1
+            Jtensor1[:,:,i,j] = Jtemp1[:,:,ctr]-repeat(mean(Jtemp1[:,:,ctr],dims=1),q,1)-repeat(mean(Jtemp1[:,:,ctr],dims=2),1,q) .+ mean(Jtemp1[:,:,ctr])
+            Jtensor1[:,:,j,i] = Jtensor1[:,:,i,j]'
+            Jtensor2[:,:,i,j] = Jtemp2[:,:,ctr]-repeat(mean(Jtemp2[:,:,ctr],dims=1),q,1)-repeat(mean(Jtemp2[:,:,ctr],dims=2),1,q) .+ mean(Jtemp2[:,:,ctr])
+            Jtensor2[:,:,j,i] = Jtensor2[:,:,i,j]'
+        end
+    end # zerosumgauge the different tensors
+
+    Jtensor = (Jtensor1 + Jtensor2)/2
+
+    FN = compute_APC(Jtensor,N,q)
+    score = GaussDCA.compute_ranking(FN,min_separation)
+    return score, FN, Jplm, hplm
 end
--- src/utils.jl
+++ src/utils.jl
@@ -8,68 +8,6 @@ function optimfunwrapper(x::Vector, g::Vector, var)
     return PLsiteAndGradSym!(x, g, var)
 end
 
-
-function ComputeScore(Jmat::Array{Float64,2}, var::PlmVar, min_separation::Int)
-
-    q = var.q
-    N = var.N
-
-    JJ=reshape(Jmat[1:end-q,:], q,q,N-1,N)
-    Jtemp1=zeros( q,q,Int(N*(N-1)/2))
-    Jtemp2=zeros( q,q,Int(N*(N-1)/2))
-    l = 1
-    for i=1:(N-1)
-        for j=(i+1):N
-            Jtemp1[:,:,l]=JJ[:,:,j-1,i]; #J_ij as estimated from from g_i.
-            Jtemp2[:,:,l]=JJ[:,:,i,j]'; #J_ij as estimated from from g_j.
-            l=l+1;
-        end
-    end
-
-
-
-
-    hplm = fill(0.0, q,N)
-    for i in 1:N
-        hplm[:,i] .= Jmat[end-q+1:end,i]
-    end
-
-    Jtensor1 = inflate_matrix(Jtemp1,N)
-    Jtensor2 = inflate_matrix(Jtemp2,N)
-    Jplm = (Jtensor1 + Jtensor2)/2 # for the energy I do not want to gauge
-
-    ctr = 0
-    for i in 1:N-1
-        for j in i+1:N
-            ctr += 1
-            Jtensor1[:,:,i,j] = Jtemp1[:,:,ctr]-repeat(mean(Jtemp1[:,:,ctr],dims=1),q,1)-repeat(mean(Jtemp1[:,:,ctr],dims=2),1,q) .+ mean(Jtemp1[:,:,ctr])
-            Jtensor1[:,:,j,i] = Jtensor1[:,:,i,j]'
-            Jtensor2[:,:,i,j] = Jtemp2[:,:,ctr]-repeat(mean(Jtemp2[:,:,ctr],dims=1),q,1)-repeat(mean(Jtemp2[:,:,ctr],dims=2),1,q) .+ mean(Jtemp2[:,:,ctr])
-            Jtensor2[:,:,j,i] = Jtensor2[:,:,i,j]'
-        end
-    end # zerosumgauge the different tensors
-
-    Jtensor = (Jtensor1 + Jtensor2)/2
-
-    FN = compute_APC(Jtensor,N,q)
-    score = GaussDCA.compute_ranking(FN,min_separation)
-    return score, FN, Jplm, hplm
-end
-
-
-function compute_APC(J::Array{Float64,4},N,q)
-    FN = fill(0.0, N,N)
-    for i=1:N-1
-        for j=i+1:N
-            FN[i,j] = norm(J[1:q-1,1:q-1,i,j],2)
-            FN[j,i] =FN[i,j]
-        end
-    end
-    FN=GaussDCA.correct_APC(FN)
-    return FN
-end
-
-
 function inflate_matrix(J::Array{Float64,3},N)
     q,q,NN = size(J)
 
@@ -88,6 +26,17 @@ function inflate_matrix(J::Array{Float64,3},N)
 end
 
 
+function compute_APC(J::Array{Float64,4},N,q)
+    FN = fill(0.0, N,N)
+    for i=1:N-1
+        for j=i+1:N
+            FN[i,j] = norm(J[1:q-1,1:q-1,i,j],2)
+            FN[j,i] =FN[i,j]
+        end
+    end
+    FN=GaussDCA.correct_APC(FN)
+    return FN
+end
 
 
 function ReadFasta(filename::AbstractString,max_gap_fraction::Real, theta::Any, remove_dups::Bool)
