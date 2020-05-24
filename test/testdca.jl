7742c48e054bd5a649f7c16c4f33e2ff87bcd7a9
Andrea Pagnani <andrea.pagnani@gmail.com>
2020-05-24 18:48:13 +0200

    return good gauge for asymmetric ... still not working

--- test/testdca.jl
+++ test/testdca.jl
@@ -48,25 +48,54 @@ end
 
 function testDCA(N,q;
                  verbose::Bool=false,
-                 epsconv::Real=1e20, gauge = ZeroSumGauge(),
+                 epsconv::Real=1e20,
+                 gauge = ZeroSumGauge(),
                  lambdaJ::Real = 0.0,
                  lambdaH=0.0,
                  asym::Bool=true,
                  maxit::Integer=1000,
-                 method::Symbol=:LD_LBFGS)
-   
+                 method::Symbol=:LD_LBFGS,
+                 epstest::Real=1e-6)
+
     W,Z,J,h = generateWZJh(N,q)
 
     func_dca = asym ? plmdca_asym : plmdca_sym
-    
-    resplm = func_dca(Z,W,lambdaJ=lambdaJ,lambdaH=lambdaH,epsconv=epsconv,verbose=verbose,maxit=maxit,method=method)
-   
+
+    resplm = func_dca(Z,W,
+                    lambdaJ=lambdaJ,
+                    lambdaH=lambdaH,
+                    epsconv=epsconv,
+                    verbose=verbose,
+                    maxit=maxit,
+                    method=method)
+
     Jplm,hplm = resplm.Jtensor,resplm.htensor
     Jz,hz = PottsGauge.gauge(J,h,gauge)
     Jplmz,hplmz = PottsGauge.gauge(Jplm,hplm,gauge)
 
-    @test sum(abs2,Jz-Jplmz)<1e-6
-    @test sum(abs2,hz-hplmz)<1e-6
+    if asym
+        if lambdaJ == 0 && lambdaH == 0 # J h are equal only for lambdas = 0!!
+            @test  sum(abs2,Jz-Jplmz)<epstest
+            @test  sum(abs2,hz-hplmz)<epstest
+        end
+
+        # test symmetric gauge
+        # \lambda_J \sum_{b}J_{i,j}(a,b) == \lambda_H h_i(a)
+        # \lambda_J \sum_{a}J_{i,j}(a,b) == \lambda_H h_j(b)
+        for i in 1:N-1
+            for j in i+1:N
+                @test sum(abs2,lambdaJ * sum(Jplm[:,:,i,j],dims=1)' .- lambdaH *hplm[:,j]) < epstest
+                @test sum(abs2,lambdaJ * sum(Jplm[:,:,i,j],dims=2)  .- lambdaH *hplm[:,i]) < epstest
+            end
+        end
+        # sum_a h_i(a) = 0
+        for i in 1:N
+            @test sum(abs2,hplm[:,i])<epstest
+        end
+    else # symmetric case
+        @test  sum(abs2,Jz-Jplm)<epstest
+        @test  sum(abs2,hz-hplm)<epstest
+    end
     nothing
 end
 for tf in (true, false) # asymmetric (true) and symmetric (false
