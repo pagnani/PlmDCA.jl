694e7d780e8d0aa4dcb2f29ff7c1cec4c695863a
Guido <guidouguzzoni@yahoo.it>
2020-11-30 14:08:25 +0100

    loopvectorization, ftol

--- src/plmdca_asym.jl
+++ src/plmdca_asym.jl
@@ -51,7 +51,7 @@ function MinimizePLAsym(alg::PlmAlg, var::PlmVar)
     Jmat = @distributed hcat for site=1:var.N #1:12
 
         opt = Opt(alg.method, length(x0))
-        xtol_abs!(opt, alg.epsconv)
+        ftol_abs!(opt, alg.epsconv)
         xtol_rel!(opt, alg.epsconv)
         xtol_abs!(opt, alg.epsconv)
         ftol_rel!(opt, alg.epsconv)
@@ -93,8 +93,8 @@ function ComputeUL(alg::PlmAlg, var::PlmVar, site::Int, LL::Int)
     return lb,ub
 end
 
-############################### NEW
 #= 
+############################### NEW 
 function PLsiteAndGrad!(x::Vector{Float64}, grad::Vector{Float64}, site::Int, plmvar::PlmVar)
 
 
@@ -120,34 +120,38 @@ function PLsiteAndGrad!(x::Vector{Float64}, grad::Vector{Float64}, site::Int, pl
 	vecene = zeros(Float64,q)
 	lnorm = 0.0
 	expvecenesumnorm = zeros(Float64,q)
+   
+    @inbounds @simd for m = 1:M
 
-   @inbounds @simd for m = 1:M
+            izm = view(IdxZ,:,m)
+            zsm = Z[site,m]
 
-			fillvecene!(vecene, x, site, view(IdxZ,:,m), q, N)
+			fillvecene!(vecene, x, site, izm , q, N)
 	        lnorm = logsumexp(vecene) #lnorm = log(sumexp(vecene))
 	        expvecenesumnorm = @. exp(vecene - lnorm) #expvecenesunorm = exp.(vecene .- lnorm)
-	        pseudolike -= W[m] * (vecene[Z[site,m]] - lnorm)
+	        pseudolike -= W[m] * (vecene[ zsm ] - lnorm)
 	        @simd for i = 1:site-1
 	            @simd for s = 1:q
-	                grad[ IdxZ[i,m] + s ] += W[m] * expvecenesumnorm[s]
+	                grad[ izm[i] + s ] += W[m] * expvecenesumnorm[s]
 	            end
-	            grad[ IdxZ[i,m] + Z[site,m] ] -= W[m]
+	            grad[ izm[i] + zsm ] -= W[m]
 	        end
             @simd for i = site+1:N
 	            @simd for s = 1:q
-	                grad[ IdxZ[i,m] - q2 + s ] += W[m] *  expvecenesumnorm[s]
+	                grad[ izm[i] - q2 + s ] += W[m] *  expvecenesumnorm[s]
 	            end
-	            grad[ IdxZ[i,m] - q2 + Z[site,m] ] -= W[m]
+	            grad[ izm[i] - q2 + zsm ] -= W[m]
 	        end
 	        @simd for s = 1:q
 	            grad[ (N-1) * q2 + s ] += W[m] * expvecenesumnorm[s]
 	        end
-			grad[ (N-1) * q2 + Z[site,m] ] -= W[m]
+			grad[ (N-1) * q2 + zsm ] -= W[m]
 	end
 
+
 	pseudolike += L2norm_asym(x, plmvar)
-    #return pseudolike
 end =#
+
 
 
 ############################### NEW @avx
@@ -204,66 +208,8 @@ function PLsiteAndGrad!(x::Vector{Float64}, grad::Vector{Float64}, site::Int, pl
 			grad[ (N-1) * q2 + zsm ] -= W[m]
 	end
 
-
 	pseudolike += L2norm_asym(x, plmvar)
-    #return pseudolike
 end
-
-
-#= 
-function PLsiteAndGrad!(vecJ::Array{Float64,1},  grad::Array{Float64,1}, site::Int, plmvar::PlmVar)
-
-    LL = length(vecJ)
-    q2 = plmvar.q2
-    q = plmvar.q
-    N = plmvar.N
-    M = plmvar.M
-    Z = sdata(plmvar.Z)
-    W = sdata(plmvar.W)
-
-    for i=1:LL-q
-        grad[i] = 2.0 * plmvar.lambdaJ * vecJ[i]
-    end
-    for i=(LL-q+1):LL
-       grad[i] = 4.0 * plmvar.lambdaH * vecJ[i]
-    end
-
-    vecene = zeros(Float64,q)
-    expvecenesunorm = zeros(Float64,q)
-    pseudolike = 0.0
-
-    @inbounds for a = 1:M
-        fillvecene!(vecene, vecJ,site,a, q, Z,N)
-        lnorm = log(sumexp(vecene))
-        expvecenesunorm .= exp.(vecene .- lnorm)
-        pseudolike -= W[a] * (vecene[Z[site,a]] - lnorm)
-        offset = 0
-        fact = 1.0
-        for i = 1:site-1
-            @simd for s = 1:q
-                grad[ offset + s + q * ( Z[i,a] - 1 ) ] += fact * W[a] *  expvecenesunorm[s]
-            end
-            grad[ offset + Z[site,a] + q * ( Z[i,a] - 1 ) ] -= fact*W[a]
-            offset += q2
-        end
-		for i = site+1:N
-            @simd for s = 1:q
-                grad[ offset + s + q * ( Z[i,a] - 1 ) ] += fact * W[a] *  expvecenesunorm[s]
-            end
-            grad[ offset + Z[site,a] + q * ( Z[i,a] - 1 ) ] -= fact *W[a]
-            offset += q2
-        end
-
-        @simd for s = 1:q
-            grad[ offset + s ] += W[a] *  expvecenesunorm[s]
-        end
-	grad[ offset + Z[site,a] ] -= W[a]
-    end
-
-    pseudolike += L2norm_asym(vecJ, plmvar)
-
-    return pseudolike
-end =#
 
 
 #Energy filling
@@ -320,33 +266,6 @@ function L2norm_asym(vec::Array{Float64,1}, plmvar::PlmVar)
     return mysum1+mysum2
 end
 
-
-
-
-#= 
-function fillvecene!(vecene::Array{Float64,1}, vecJ::Array{Float64,1},site::Int, a::Int, q::Int, sZ::DenseArray{Int,2},N::Int)
-
-    q2 = q*q
-    Z = sdata(sZ)
-
-    @inbounds for l = 1:q
-        offset::Int = 0
-        scra::Float64 = 0.0
-        for i = 1:site-1 # Begin sum_i \neq site J
-            scra += vecJ[offset + l + q * (Z[i,a]-1)]
-            offset += q2
-        end
-        # skipping sum over residue site
-    	for i = site+1:N
-            scra += vecJ[offset + l + q * (Z[i,a]-1)]
-            offset += q2
-        end # End sum_i \neq site J
-
-        scra += vecJ[offset + l] # sum H
-        vecene[l] = scra
-    end
-    return
-end =#
 
 
 function ComputeScore(Jmat::Array{Float64,2}, var::PlmVar, min_separation::Int)
