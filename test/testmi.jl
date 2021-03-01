8413066abacffecb7a5a63a300e85912b45ef718
Andrea Pagnani <pagnani@medusa.polito.it>
2021-03-01 16:14:51 +0100

    fixed test

--- test/testmi.jl
+++ test/testmi.jl
@@ -1,8 +1,7 @@
 module TestMI
 using Test, PlmDCA
-# MI = PlmDCA.mutualinfo("test.fasta",pseudocount=0)
-#@test MI == zero(MI)
+MI = PlmDCA.mutualinfo("test.fasta",pseudocount=0)
+@test MI == zero(MI)
 @test true
-#printstyled("All TestMI passed!\n",color=:light_green,bold=true)
-printstyled("TestMI waiting that https://github.com/carlobaldassi/DCAUtils.jl/issues/2 is fixed\n",color=:light_green,bold=true)
+printstyled("All TestMI passed!\n",color=:light_green,bold=true)
 end
