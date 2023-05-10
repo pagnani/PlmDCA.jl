bdd319c67795d4fc7aac31d83563899d8beb247f
Andrea Pagnani <andrea.pagnani@gmail.com>
2023-05-10 16:18:40 +0200

    wip

--- src/PlmDCA.jl
+++ src/PlmDCA.jl
@@ -1,7 +1,7 @@
 module PlmDCA
 import Printf:@printf
 import LinearAlgebra:norm, rmul!
-import Statistics
+import Statistics:mean
 using NLopt
 import DCAUtils: read_fasta_alignment, remove_duplicate_sequences, compute_weights, add_pseudocount, compute_weighted_frequencies
 using LoopVectorization
